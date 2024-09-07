05226167046246f760d98f12fa0b6231e0e60dc4
awab habiballa <awab.alabady@gmail.com>
2024-09-07 19:11:42 +0400

    add image slider to the listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwipeCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+export default function Listing() {
+  SwipeCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setError(false);
+        setLoading(true);
+
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [params.listingId]);
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <div className="flex flex-col items-center">
+          <p className="text-center my-7 text-2xl">Something went wrong :) </p>
+          <Link to="/">
+            <span className="text-blue-600">Back to home</span>
+          </Link>
+        </div>
+      )}
+
+      {listing && !loading && !error && (
+        <>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+    </main>
+  );
+}
