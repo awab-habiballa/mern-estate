4b9b9ba74691ebcf1ed6c822b7121609522e7426
awab habiballa <awab.alabady@gmail.com>
2024-09-12 20:54:26 +0400

    update the home ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
     };
     const fetchRentListings = async () => {
       try {
-        const res = await fetch("/api/listing/get?type=rent&limit=5");
+        const res = await fetch("/api/listing/get?type=rent&limit=4");
         const data = await res.json();
         setRentListings(data);
         fetchSaleListings();
@@ -36,7 +36,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch("/api/listing/get?type=sell&limit=5");
+        const res = await fetch("/api/listing/get?type=sell&limit=4");
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
@@ -47,27 +47,6 @@ export default function Home() {
   }, []);
   return (
     <div>
-      {/* top */}
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
-          <br />
-          place with ease
-        </h1>
-        <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
-          live.
-          <br />
-          We have a wide range of properties for you to choose from.
-        </div>
-        <Link
-          to={"/search"}
-          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
-        >
-          Let's get started...
-        </Link>
-      </div>
-
       {/* swiper */}
       <Swiper navigation>
         {offerListings &&
@@ -79,19 +58,38 @@ export default function Home() {
                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
                   backgroundSize: "cover",
                 }}
-                className="h-[500px] mx-4"
+                className="h-[500px] "
                 key={listing._id}
               ></div>
             </SwiperSlide>
           ))}
       </Swiper>
+      {/* top */}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Awab Estate is the best place to find your next perfect place to live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-xl text-blue-800 font-bold hover:underline"
+        >
+          Let's get started...
+        </Link>
+      </div>
 
       {/* listing results for offer, sale and rent */}
 
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-1">
         {offerListings && offerListings.length > 0 && (
           <div className="">
-            <div className="my-3">
+            <div className="my-1">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent offers
               </h2>
